e5e63654d1582bc71d54c3d154037bbec6f6361f
ZexI <75762009+JiaHao-YU03@users.noreply.github.com>
2022-07-29 11:03:00 +0800

    Update main.cpp

--- HIK_OPENCV_colDet/main.cpp
+++ HIK_OPENCV_colDet/main.cpp
@@ -11,13 +11,13 @@ RIFrameInfo depth = { 0 };
 RIFrameInfo rgb = { 0 };
 
 Mat frame, blur_frame, dil_frame, ero_frame, after_frame;
-//Ô¤ÉèËÄÉ«µÄhsv·¶Î§Öµ
+//é¢„è®¾å››è‰²çš„hsvèŒƒå›´å€¼
 vector<vector<int>> ColorValues{ {0, 18, 0, 179, 255, 62},      //black
                                                     {80, 43, 46, 124, 255, 255},    //blue
                                                     {0, 43, 46, 5, 255, 255},          //red
                                                     {35, 43, 46, 77, 255, 255} };   //green
 
-//³õÊ¼»¯+¿ªÆôÉè±¸
+//åˆå§‹åŒ–+å¼€å¯è®¾å¤‡
 void HIK_initialization()
 {
     MV3D_RGBD_VERSION_INFO stVersion;
@@ -31,7 +31,7 @@ void HIK_initialization()
     LOGD("MV3D_RGBD_GetDeviceNumber success! nDevNum:%d.", nDevNum);
     ASSERT(nDevNum);
 
-    // ²éÕÒÉè±¸
+    // æŸ¥æ‰¾è®¾å¤‡
     std::vector<MV3D_RGBD_DEVICE_INFO> devs(nDevNum);
     ASSERT_OK(MV3D_RGBD_GetDeviceList(DeviceType_Ethernet | DeviceType_USB, &devs[0], nDevNum, &nDevNum));
     for (unsigned int i = 0; i < nDevNum; i++)
@@ -39,17 +39,17 @@ void HIK_initialization()
         LOG("Index[%d]. SerialNum[%s] IP[%s] name[%s].\r\n", i, devs[i].chSerialNumber, devs[i].SpecialInfo.stNetInfo.chCurrentIp, devs[i].chModelName);
     }
 
-    //´ò¿ªÉè±¸
+    //æ‰“å¼€è®¾å¤‡
     unsigned int nIndex = 0;
     ASSERT_OK(MV3D_RGBD_OpenDevice(&handle, &devs[nIndex]));
     LOGD("OpenDevice success.");
 
-    // ¿ªÊ¼¹¤×÷Á÷³Ì
+    // å¼€å§‹å·¥ä½œæµç¨‹
     ASSERT_OK(MV3D_RGBD_Start(handle));
     LOGD("Start work success.");
 }
 
-//¹Ø±ÕÊÍ·ÅÉè±¸
+//å…³é—­é‡Šæ”¾è®¾å¤‡
 void HIK_stop()
 {
     ASSERT_OK(MV3D_RGBD_Stop(handle));
@@ -82,11 +82,13 @@ String coltypes(int type)
 
 Point getcontours(Mat image)
 {
+    //è½®å»“ç‚¹å‘é‡contours
     vector<vector<Point>> contours;
     vector<Vec4i> hierarchy;
 
     findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
+    //çŸ©å½¢ç‚¹å‘é‡å’ŒçŸ©å½¢å‘é‡
     vector<vector<Point>> conpoly(contours.size());
     vector<Rect> boundirect(contours.size());
 
@@ -96,25 +98,27 @@ Point getcontours(Mat image)
     {
         int area = contourArea(contours[i]);
 
+        //æ§åˆ¶æ£€æµ‹åƒç´ é¢ç§¯å¤§å°
         if (area> 3000 && area < 10000)
         {
-            //¶à±ßĞÎÇúÏßÄâºÏ
+            //å¤šè¾¹å½¢æ›²çº¿æ‹Ÿåˆ
             float peri = arcLength(contours[i], true);
-            approxPolyDP(contours[i], conpoly[i], 0.02 * peri, true);
+            approxPolyDP(contours[i], conpoly[i], 0.03 * peri, true);
 
             LOGD("contours: area(%d);  conpoly[%d]: size(%d)", area, i, conpoly[i].size());
 
             boundirect[i] = boundingRect(conpoly[i]);
 
-            //Í¼¿òµÄ×óÉÏ½Ç
+            //å›¾æ¡†çš„å·¦ä¸Šè§’
             myPoint.x = boundirect[i].x;
             myPoint.y = boundirect[i].y;
 
+            //å›¾åƒç»˜å›¾
             //drawContours(frame, conpoly, i, Scalar(255, 0, 255), 2);
-            rectangle(after_frame, boundirect[i].tl(), boundirect[i].br(), Scalar(0, 0, 255), 2);
+            rectangle(after_frame, boundirect[i].tl(), boundirect[i].br(), Scalar(0, 0, 255), 2);  
+            LOGD("draw finish!");
         }
     }
-    LOGD("draw finish!");
 
     return myPoint;
 }
@@ -122,7 +126,7 @@ Point getcontours(Mat image)
 void getcolor(Mat ero_frame)
 {
     Mat hsv_frame;
-    cvtColor(ero_frame, hsv_frame, COLOR_BGR2HSV);
+    cvtColor(ero_frame, hsv_frame, COLOR_RGB2HSV);
 
     for (int i = 0; i < ColorValues.size(); i++)
     {
@@ -130,7 +134,7 @@ void getcolor(Mat ero_frame)
         Scalar lower(ColorValues[i][0], ColorValues[i][1], ColorValues[i][2]);
         Scalar upper(ColorValues[i][3], ColorValues[i][4], ColorValues[i][5]);
         
-        //¶şÖµ»¯Í¼Ïñ
+        //äºŒå€¼åŒ–å›¾åƒ
         inRange(hsv_frame, lower, upper, mask);
 
         Point po = getcontours(mask);
@@ -147,33 +151,34 @@ void getcolor(Mat ero_frame)
 
 int main(int argc, char** argv)
 {
+    //åˆå§‹åŒ–ç›¸æœºå¹¶æ‰“å¼€
     HIK_initialization();
 
     MV3D_RGBD_FRAME_DATA stFrameData = { 0 };
 
     while (1)
     {
-        // »ñÈ¡Í¼ÏñÊı¾İ
+        // è·å–å›¾åƒæ•°æ®
         int nRet = MV3D_RGBD_FetchFrame(handle, &stFrameData, 5000);
         if (MV3D_RGBD_OK == nRet)
         {
-            //·ÖÎö»ñÈ¡Ã¿Ö¡Êı¾İ
+            //åˆ†æè·å–æ¯å¸§æ•°æ®
             parseFrame(&stFrameData, &depth, &rgb);
 
-            LOGD("rgb: FrameNum(%d), height(%d), width(%d)¡£", rgb.nFrameNum, rgb.nHeight, rgb.nWidth);
+            LOGD("rgb: FrameNum(%d), height(%d), width(%d)ã€‚", rgb.nFrameNum, rgb.nHeight, rgb.nWidth);
             
-            //×ª»»Á½¸öÏàÍ¬Í¼Ïñ£¬Ò»¸ö´¦Àí£¬Ò»¸ö»æÍ¼
+            //è½¬æ¢ä¸¤ä¸ªç›¸åŒå›¾åƒï¼Œä¸€ä¸ªå¤„ç†ï¼Œä¸€ä¸ªç»˜å›¾
             Mat rgb_frame(rgb.nHeight, rgb.nWidth, CV_8UC3, rgb.pData);
-            cvtColor(rgb_frame, frame, COLOR_RGB2BGR);
-            cvtColor(rgb_frame, after_frame, COLOR_RGB2BGR);
+            cvtColor(rgb_frame, frame, COLOR_BGR2RGB);
+            cvtColor(rgb_frame, after_frame, COLOR_BGR2RGB);
 
-            //Æ½»¬´¦Àí£¬À©´óÔÙÇÖÊ´
+            //å¹³æ»‘å¤„ç†ï¼Œæ‰©å¤§å†ä¾µèš€
             GaussianBlur(frame, blur_frame, Size(7, 7), 5, 0);
             Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
             dilate(blur_frame, dil_frame, kernel);
             erode(dil_frame, ero_frame, kernel);
 
-            //Ñ°ÕÒĞ¡¿éÑÕÉ«
+            //å¯»æ‰¾å°å—é¢œè‰²
             getcolor(ero_frame);
 
             namedWindow("color_detection", 0);
@@ -184,6 +189,7 @@ int main(int argc, char** argv)
         }
     }
 
+    //ç›¸æœºå…³é—­ï¼Œçº¿ç¨‹é‡Šæ”¾
     HIK_stop();
 
    return  0;
